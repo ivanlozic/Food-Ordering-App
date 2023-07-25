adaf42a046ae8f7a3acaaf1332662a51ada49202
Ivan Lozic <ivanlozic995@gmail.com>
2023-07-25 17:21:00 +0200

    Navbar fixes

--- src/components/navigation/navbar/Navbar.js
+++ src/components/navigation/navbar/Navbar.js
@@ -1,29 +1,28 @@
-import classes from "./Navbar.module.css";
-import { useSelector } from "react-redux";
-import { useState } from "react";
+import classes from './Navbar.module.css'
+import { useSelector } from 'react-redux'
+import { useState } from 'react'
 import Cart from '../cart/Cart'
-import SideModal from "../side-modal/SideModal";
-import logo from "../../../assets/images/logo.jpg";
-import React from "react";
-import Info from "../../info/Info";
+import SideModal from '../side-modal/SideModal'
+import logo from '../../../assets/images/logo.jpg'
+import React from 'react'
 
 const Navbar = () => {
-  const cart = useSelector((state) => state.cart);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const cart = useSelector((state) => state.cart)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const handleCartClick = () => {
-    setIsModalOpen(true);
-  };
+    setIsModalOpen(true)
+  }
 
   const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+  }
 
   return (
     <div className={classes.Navbar}>
       <div className={classes.nav}>
         <div className={classes.headings}>
-          <img src={logo} alt="logo" />
+          <img src={logo} alt='logo' />
           <div>
             <h2>Chicken chill</h2>
             <h4>Feel the chill</h4>
@@ -35,35 +34,31 @@ const Navbar = () => {
         <SideModal isOpen={isModalOpen} onClose={handleCloseModal} />
       </div>
 
-
       <ul className={classes.ul}>
         <li className={classes.li}>
-          <a href="#pasta">Pasta meat</a>
+          <a href='#pasta'>Pasta meat</a>
         </li>
         <li className={classes.li}>
-          <a href="#popcorn">Popcorn</a>
+          <a href='#popcorn'>Popcorn</a>
         </li>
         <li className={classes.li}>
-          <a href="#fries-meat">Fries meat</a>
+          <a href='#fries-meat'>Fries meat</a>
         </li>
         <li className={classes.li}>
-          <a href="#burgers">Burgers</a>
+          <a href='#burgers'>Burgers</a>
         </li>
         <li className={classes.li}>
-          <a href="#dogs">Dogs</a>
+          <a href='#dogs'>Dogs</a>
         </li>
         <li className={classes.li}>
-          <a href="#other">Other</a>
+          <a href='#other'>Other</a>
         </li>
         <li className={classes.li}>
-          <a href="#drinks">Drinks</a>
+          <a href='#drinks'>Drinks</a>
         </li>
       </ul>
-
-
-      <Info />
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
