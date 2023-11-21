b1df0670b6f35a142516032c47f3da21f46432a6
Ivan Lozic <ivanlozic995@gmail.com>
2023-11-21 09:25:24 -0500

    Average rating user review

--- src/components/user-review-section/UserReviewSection.jsx
+++ src/components/user-review-section/UserReviewSection.jsx
@@ -14,57 +14,71 @@ const UserReviewSection = () => {
   const [currentSlide, setCurrentSlide] = useState(0)
   const [showReviewModal, setShowReviewModal] = useState(false)
   const user = useSelector((state) => state.user)
-  let itemsPerSlide = determineItemsPerSlide();
+  let itemsPerSlide = determineItemsPerSlide()
 
   useEffect(() => {
     const fetchReviews = async () => {
       try {
         const response = await axiosInstance.get(
           axiosRoutes.userReview.getAllReviews
-        );
+        )
 
-        setReviews(response.data.data.reviews);
-        console.log(response.data.data);
-        console.log(reviews);
+        setReviews(response.data.data.reviews)
+        console.log(response.data.data)
+        console.log(reviews)
       } catch (error) {
-        console.error('Error fetching reviews:', error);
+        console.error('Error fetching reviews:', error)
       }
-    };
+    }
 
-    fetchReviews();
+    fetchReviews()
 
-    // Update itemsPerSlide on initial render and window resize
     const updateItemsPerSlide = () => {
-      itemsPerSlide = determineItemsPerSlide();
-      setCurrentSlide(0); 
-    };
+      itemsPerSlide = determineItemsPerSlide()
+      setCurrentSlide(0)
+    }
 
-    updateItemsPerSlide();
+    updateItemsPerSlide()
 
-    window.addEventListener('resize', updateItemsPerSlide);
+    window.addEventListener('resize', updateItemsPerSlide)
 
     return () => {
-      window.removeEventListener('resize', updateItemsPerSlide);
-    };
-  }, [reviews.length]); 
+      window.removeEventListener('resize', updateItemsPerSlide)
+    }
+  }, [reviews.length])
+  const calculateAverageVote = () => {
+    if (reviews.length === 0) {
+      return 0
+    }
+    const totalVotes = reviews.reduce((acc, review) => {
+      const starRating = parseFloat(review.Stars);
+      return isNaN(starRating) ? acc : acc + starRating;
+    }, 0);
+  
+    const averageVote = totalVotes / reviews.length;
+  
+    return Math.round(averageVote * 10) / 10;
+  };
 
-  const startReviewIndex = currentSlide * itemsPerSlide;
+  const averageVote = calculateAverageVote()
+
+  const startReviewIndex = currentSlide * itemsPerSlide
   const endReviewIndex = Math.min(
     currentSlide * itemsPerSlide + itemsPerSlide,
     reviews.length
-  );
+  )
   const currentReviews = Array.isArray(reviews)
     ? reviews.slice(startReviewIndex, endReviewIndex)
-    : [];
+    : []
 
   const nextSlide = () => {
-    const totalSlides = Math.ceil(reviews.length / itemsPerSlide);
-    const nextIndex = (currentSlide + 1) % totalSlides;
-    setCurrentSlide(nextIndex);
-  };
+    const totalSlides = Math.ceil(reviews.length / itemsPerSlide)
+    const nextIndex = (currentSlide + 1) % totalSlides
+    setCurrentSlide(nextIndex)
+  }
 
   function determineItemsPerSlide() {
-    return window.innerWidth < 768 ? 1 : 3;
+    return window.innerWidth < 768 ? 1 : 3
   }
   const prevSlide = () => {
     if (currentSlide > 0) {
@@ -85,6 +99,9 @@ const UserReviewSection = () => {
   return (
     <div className={styles.userReviewSection}>
       <h2>User Reviews</h2>
+      <div className={styles.averageVote}>
+        <p>Average Rating: {averageVote}</p>
+      </div>
       <div className={styles.slider}>
         <div className={styles.arrowLeft} onClick={prevSlide}>
           &#8592;
